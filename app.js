9371b2e3956a190127dccfea928609e887e23f71
Jc <joaocpmcs@gmail.com>
2024-01-04 13:46:03 -0300

    modularizado

--- app.js
+++ app.js
@@ -1,72 +1,68 @@
-let pessoas = []
-let input = document.getElementById('search')
-let form = document.querySelector('.search')
-let app = document.querySelector('.dados')
+   export function Factory({app}){
+    let pessoas = []
 
-
-
-if(!(localStorage.getItem('pessoas') == 'null')){
-    pessoas = JSON.parse(localStorage.getItem('pessoas'))
-    update()
-}
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    request(input.value)
-    input.value = ''
-} )
-
-async function request(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    if(!response.ok){
-        throw 'Erro na request'
+    if(!(localStorage.getItem('pessoas') == null)){
+        pessoas = JSON.parse(localStorage.getItem('pessoas'))
+        update()
     }
-   let resposta = await response.json()
-   resposta.cssId = pessoas.length
-   pessoas.push(resposta)
-    update()
-}
 
-function criar(resposta) {
-    let htmlContent = `
-    <div class="row">
-            <div class="content">
-                <img src="${resposta.avatar_url}">
-                <div>
-                    <h1>${resposta.login}</h1>
-                    <p>${resposta.name}</p>
+    async function request(userName) {
+        const response = await fetch(`https://api.github.com/users/${userName}`)
+
+        if(!response.ok){
+            throw 'Erro na request'
+        }
+
+       let resposta = await response.json()
+       resposta.cssId = pessoas.length
+       pessoas.push(resposta)
+       update()
+    }
+
+    function update() {    
+        localStorage.setItem('pessoas', JSON.stringify(pessoas))
+        app.innerHTML = ""
+        let idNumber = 0
+        for(let entry of pessoas){
+            entry.idCss = idNumber
+            idNumber++
+            criar(entry, idNumber)
+        }  
+        let arr = document.querySelectorAll('i')
+        arr.forEach(element => {
+            element.addEventListener('click', () => remove(element.id))
+        })
+    }
+    
+    function criar(resposta) {
+        let htmlContent = `
+        <div class="row">
+                <div class="content">
+                    <img src="${resposta.avatar_url}">
+                    <div>
+                        <h1>${resposta.login}</h1>
+                        <p>${resposta.name}</p>
+                    </div>
                 </div>
-            </div>
-
-            <div class="content">
-                <p>${resposta.public_repos}</p>
-            </div>
-            <div class="content">
-                <div></div>
-                <p>${resposta.followers}</p>
-                <i class="fa-solid fa-xmark" onclick="remove(${resposta.idCss})"></i>
-            </div>
-    </div>`
-  
-    app.innerHTML += htmlContent
-}
-
-function remove(element) {
-    pessoas.splice(element, 1)
-    update()
-}
-
-function update() {    
-    localStorage.setItem('pessoas', JSON.stringify  (pessoas))
-    app.innerHTML = ""
-    let idNumber = 0
-    for(entry of pessoas){
-        entry.idCss = idNumber
-        idNumber++
-        criar(entry, idNumber)
+    
+                <div class="content">
+                    <p>${resposta.public_repos}</p>
+                </div>
+                <div class="content">
+                    <div></div>
+                    <p>${resposta.followers}</p>
+                    <i class="fa-solid fa-xmark" id="${resposta.idCss}"></i>
+                </div>
+        </div>`
+      
+        app.innerHTML += htmlContent
     }
+    
+    function remove(element) {
+        pessoas.splice(element, 1)
+        update()
+    }
+    
+   
+    return { update, remove, request }
 }
-
-
-
-
---
+++ main.js
@@ -0,0 +1,27 @@
+import {Factory} from "./app.js"
+
+
+
+let input = document.getElementById('search')
+let form = document.querySelector('.search')
+let app = document.querySelector('.dados')
+let geral
+
+
+geral = Factory({
+    app})
+
+
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    geral.request(input.value)
+    input.value = ''
+} )
+
+
+
+
+
